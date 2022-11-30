5c37625cf48be352adcdf898ea6c782c83cc1179
PMKrol <88658026+PMKrol@users.noreply.github.com>
2022-11-30 16:24:27 +0100

    Create heaven.sh

---
+++ heaven.sh
@@ -0,0 +1,7 @@
+cd ~/.phoronix-test-suite/installed-tests/pts/unigine-heaven-1.6.5/Unigine_Heaven-4.0/bin/
+#./heaven_x64 \
+#-data_path .. \
+#-sound_app null \
+#-system_script heaven/unigine.cpp \
+#-video_width 1280 -video_height 1024 -video_fullscreen 0 -video_app opengl 
+./heaven_x64 -data_path ../ -sound_app null -system_script heaven/unigine.cpp -video_width 1680 -video_height 1050 -video_fullscreen 1 # -video_app opengl
