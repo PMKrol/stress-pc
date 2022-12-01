6ca7556d4c3cb1ce3306d9002635d8d04c111c89
PMKrol <88658026+PMKrol@users.noreply.github.com>
2022-12-01 10:56:52 +0100

    Update stress-pc.sh

--- stress-pc.sh
+++ stress-pc.sh
@@ -4,4 +4,6 @@
 #stress --cpu 8 | while true; do phoronix-test-suite batch-run unigine-heaven; done | memtester 11G
 
 #while true; do phoronix-test-suite batch-run unigine-heaven; done | watch "sensors && echo nVidia: &&  nvidia-smi -q | grep 'Current Temp' && nvidia-smi -q | grep Gpu"
-./stress.sh | ./heaven.sh | memtester 11G | ./watch.sh
+sudo echo "ok"
+#./stress.sh | ./heaven.sh > /dev/null | sudo memtester 10G >/dev/null | ./watch.sh
+./stress.sh | ./gputest.sh | sudo memtester 10G >/dev/null | ./watch.sh
